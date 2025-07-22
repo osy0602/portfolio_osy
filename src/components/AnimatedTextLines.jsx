c2cbb47b38af35e3015692de37ea9a1b1d7a2cdd
osy0602 <osy01036095318@gmail.com>
2025-07-22 19:09:30 +0900

    add 3d model

--- src/components/AnimatedTextLines.jsx
+++ src/components/AnimatedTextLines.jsx
@@ -1,4 +1,37 @@
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+gsap.registerPlugin(ScrollTrigger);
 export const AnimatedTextLines = ({ text, className}) => {
-    
-    return 
+    const  lineRefs = useRef([]);
+    const containerRef = useRef(null);
+    const lines = text.split("\n").filter((line) => line.trim() != "");
+    useGSAP(() => {
+        if (lineRefs.current.length > 0){
+            gsap.from(lineRefs.current, {
+            y:100,
+            opacity:0,
+            duration:1,
+            stagger:0.3,
+            ease:"back.out",
+            scrollTrigger:{
+                trigger: containerRef.current,
+            },
+            });
+        }
+    });
+
+    return (
+        <div ref={containerRef} className={className}>
+            {lines.map((line, index) => (
+                <span
+                    key={index}
+                    ref={(el) => (lineRefs.current[index] = el)}
+                    className="block leading-relaxed tracking-wide text-pretty">
+                    {line}
+                </span>
+            ))}
+        </div>
+    )
 };

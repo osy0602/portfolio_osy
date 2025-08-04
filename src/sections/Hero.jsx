945b9478d90109e5c2f01c435022d9c9bffe8d4a
osy0602 <osy01036095318@gmail.com>
2025-08-04 18:26:43 +0900

    change 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,50 +1,73 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree } from '@react-three/fiber';
 import { City } from "../components/City";
 import { Environment, Float, Lightformer } from '@react-three/drei';
 import { useMediaQuery } from 'react-responsive';
+import { useEffect } from 'react';
 import AnimatedHeaderSection from '../components/AnimatedHeaderSection';
+
+
+
+const CameraController = () => {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    camera.lookAt(0, 4, 3); 
+  }, [camera]);
+
+  return null;
+};
+
+
 const Hero = () => {
     const isMobile = useMediaQuery({maxWidth: 853});
     const text = "Passionate Frontend Developer with hands-on experience\n building AI-powered and interactive web applications.";
+    
     return (
       <section id="home" className='flex flex-col justify-end min-h-screen'>
-            <AnimatedHeaderSection subTitle={"404 No Bugs Found"} title={"Seyoung Oh"} text = {text} textColor={"text-black"}/>
-              <figure 
-                  className='absolute inset-0 -z-50' 
-                  style={{width : "100vw", height:"100vh"}}>
-                    <Canvas shadows camera ={{position:[0, 0, -10], fov:17.5, near:1, far:20 }}>
-                    <ambientLight intensity={0.5}/>
-                    <Float speed = {0.5}>
-                      <City scale = {isMobile ? 0.14: 0.2}/>
-                    </Float>
-                      <Environment resolution={256}>
-                        <group rotation={[-Math.PI / 3,4,1]}>
+          <AnimatedHeaderSection subTitle={"Flying beyond borders, Thinking beyond limits"} title={"Seyoung Oh"} text={text} textColor={"text-black"}/>
+          
+          <figure 
+              className='absolute inset-0 -z-50' 
+              style={{width : "100vw", height:"100vh"}}>
+              
+              <Canvas shadows camera={{position:[5, 8, 10], fov:30, near:1, far:50 }}>
+                  
+                  <CameraController />
+
+                  <ambientLight intensity={0.5}/>
+                  
+                  <Float speed={0.5}>
+                      <City scale={isMobile ? 3 : 5}/>
+                  </Float>
+                  
+                  <Environment resolution={256}>
+                      <group rotation={[-Math.PI / 3,4,1]}>
                           <Lightformer form={"circle"}
-                            intensity={2}
-                            position={[0, 5, -9]}
-                            scale={10}
+                              intensity={2}
+                              position={[0, 5, -9]}
+                              scale={10}
                           />
                           <Lightformer form={"circle"}
-                            intensity={2}
-                            position={[0, 3, 1]}
-                            scale={10}
+                              intensity={2}
+                              position={[0, 3, 1]}
+                              scale={10}
                           />
                           <Lightformer form={"circle"}
-                            intensity={2}
-                            position={[-5, -1, -1]}
-                            scale={10}
+                              intensity={2}
+                              position={[-5, -1, -1]}
+                              scale={10}
                           />
                           <Lightformer form={"circle"}
-                            intensity={2}
-                            position={[10, 1, 0]}
-                            scale={16}
+                              intensity={2}
+                              position={[10, 1, 0]}
+                              scale={16}
                           />
-                        </group>
-                      </Environment>
-                    </Canvas>
-                  </figure>
+                      </group>
+                  </Environment>
+              </Canvas>
+          </figure>
       </section>
     );
 };
 
-export default Hero
+export default Hero;
